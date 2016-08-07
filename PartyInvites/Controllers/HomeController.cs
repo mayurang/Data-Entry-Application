60d54364ca2b0d7deb24fb2d898eb6a8b703d3ec
Mayuran <mayurangopalan@gmail.com>
2016-08-07 11:52:59 -0400

    Adding Bootstrap to the Form

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartyInvites.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,9 +16,25 @@ namespace PartyInvites.Controllers
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View ();
         }
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {   
+            if (ModelState.IsValid)
+            {
+                //TODO: Email response to Party Organizer
+                return View("Thanks", guestResponse);
+            } else
+            {
+                // there is a validation error
+                return View();
+            }
+            
         }
     }
 }
